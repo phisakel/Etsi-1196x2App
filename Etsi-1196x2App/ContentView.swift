daa50cfbb3a17bb9ba6c56fb04f89e822bb113e0
Manos Psarakis <manos.psarakis@netcompany.com>
2026-05-20 18:00:51 +0300

    Initial Commit

---
+++ Etsi-1196x2App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Etsi-1196x2App
+//
+//  Created by PSARAKIS Manos on 20/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Etsi-1196x2App/Etsi_1196x2AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Etsi_1196x2AppApp.swift
+//  Etsi-1196x2App
+//
+//  Created by PSARAKIS Manos on 20/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Etsi_1196x2AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
